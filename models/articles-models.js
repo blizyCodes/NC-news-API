4b88d2d85d484907031648ce125016dfad784a32
donblizy <nbliziotis@icloud.com>
2022-02-15 17:35:03 +0000

    refactor article-models to async

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,14 +1,13 @@
 const db = require("../db/connection");
 
 exports.selectArticleById = async (id) => {
-  return db
-    .query("SELECT * FROM articles WHERE article_id = $1;", [id])
-    .then(({ rows }) => {
-      console.log(rows);
-      const article = rows[0];
-      if (!article) {
-        return Promise.reject({ status: 404, msg: "article not found" });
-      }
-      return article;
-    });
+  const { rows } = await db.query(
+    "SELECT * FROM articles WHERE article_id = $1;",
+    [id]
+  );
+  const article = rows[0];
+  if (!article) {
+    return Promise.reject({ status: 404, msg: "article not found" });
+  }
+  return article;
 };
