4285420c449aa3903ca498a4743ce916c85fc56e
donblizy <nbliziotis@icloud.com>
2022-02-16 12:28:35 +0000

    refactor topics and users models to send 204

--- models/users-models.js
+++ models/users-models.js
@@ -2,5 +2,7 @@ const db = require("../db/connection");
 
 exports.selectUsers = async () => {
   const { rows } = await db.query("SELECT username FROM users");
+  if (rows.length === 0)
+    return Promise.reject({ status: 204, msg: "there are no users" });
   return rows;
 };
