9aecaf426bbafca9923e63926a3ee9f9981ed00b
donblizy <nbliziotis@icloud.com>
2022-02-16 11:29:16 +0000

    solve patch article votes

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -1,8 +1,21 @@
-const { selectArticleById } = require("../models/articles-models");
+const {
+  selectArticleById,
+  updateArticlebyId,
+} = require("../models/articles-models");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id: articleId } = req.params;
   selectArticleById(articleId)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => next(err));
+};
+
+exports.patchArticleById = (req, res, next) => {
+  const { article_id: articleId } = req.params;
+  console.log(req.body);
+  updateArticlebyId(req.body, articleId)
     .then((article) => {
       res.status(200).send({ article });
     })
