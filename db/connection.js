7e7ec7e2a58e3cdf1ed843b20c99f3c416292627
donblizy <nbliziotis@icloud.com>
2022-11-14 16:08:37 +0000

    migrating db from heroku

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,16 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-
 const config =
-  ENV === 'production'
+  ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
         ssl: {
@@ -19,6 +18,5 @@ const config =
         },
       }
     : {};
-
 
 module.exports = new Pool(config);
